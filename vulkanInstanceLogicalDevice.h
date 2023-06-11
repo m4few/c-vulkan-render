0055bc83f1a2a8ea7f1bd889b29a5bd2865cb547
Astrojarma <matsaunderz@gmail.com>
2023-06-11 21:17:11 +0000

    struct ptr hack for queues + validation error: queue indices are not unique

--- vulkanInstanceLogicalDevice.h
+++ vulkanInstanceLogicalDevice.h
@@ -6,19 +6,37 @@
 #include "vulkanInstance.h"
 #include "vulkanInstancePhysicalDevice.h"
 
+// NOTE: THERES A HACK HERE TO ACCESS THESE STRUCTS AS ARRAYS
+//  IF ANYTHING WEIRD HAPPENS, ITS PROBABLY THAT
+typedef struct {
+  VkQueue graphicsQueue;
+  VkQueue presentationQueue;
+} QueueHandles;
+
+typedef struct {
+  VkDeviceQueueCreateInfo graphicsQueue;
+  VkDeviceQueueCreateInfo presentationQueue;
+} QueueCreateInfos;
+
 VkDevice deviceCreateLogical(VkPhysicalDevice *physicalDevice,
                              VkSurfaceKHR *surface) {
   VkDevice device;
   QueueFamilyIndices indices = findQueueFamilies(physicalDevice, surface);
 
   // queue creation info
-  VkDeviceQueueCreateInfo queueCreateInfo = {};
-  queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-  queueCreateInfo.queueFamilyIndex = indices.graphicsQueue.value;
-  queueCreateInfo.queueCount = 1;
+  // THE HACK STARTS HERE
+  QueueCreateInfos queueCreationInfos;
+  for (uint8_t i = 0; i < QUEUE_COUNT; i++) {
+    VkDeviceQueueCreateInfo queueCreateInfo = {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = (&(indices.graphicsQueue) + i)->value;
+    queueCreateInfo.queueCount = 1;
 
-  float queuePriority = 1.0f;
-  queueCreateInfo.pQueuePriorities = &queuePriority;
+    float queuePriority = 1.0f;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    *(&(queueCreationInfos.graphicsQueue) + i) = queueCreateInfo;
+  }
 
   // enabled logical device feature info
   VkPhysicalDeviceFeatures deviceFeatures = {};
@@ -27,8 +45,9 @@ VkDevice deviceCreateLogical(VkPhysicalDevice *physicalDevice,
   VkDeviceCreateInfo logicalDeviceCreateInfo = {};
 
   logicalDeviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-  logicalDeviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
-  logicalDeviceCreateInfo.queueCreateInfoCount = 1;
+  logicalDeviceCreateInfo.pQueueCreateInfos =
+      &(queueCreationInfos.graphicsQueue);
+  logicalDeviceCreateInfo.queueCreateInfoCount = QUEUE_COUNT;
   logicalDeviceCreateInfo.pEnabledFeatures = &deviceFeatures;
   logicalDeviceCreateInfo.enabledExtensionCount = 0;
 
@@ -49,10 +68,23 @@ VkDevice deviceCreateLogical(VkPhysicalDevice *physicalDevice,
   return device;
 }
 
-VkQueue deviceGetQueueHandles(VkPhysicalDevice *pDevice, VkDevice *lDevice,
-                              VkSurfaceKHR *surface) {
-  VkQueue graphicQueue;
+QueueHandles deviceGetQueueHandles(VkPhysicalDevice *pDevice, VkDevice *lDevice,
+                                   VkSurfaceKHR *surface) {
+  // make struct and get indices
+  QueueHandles handles = {};
   QueueFamilyIndices indices = findQueueFamilies(pDevice, surface);
-  vkGetDeviceQueue(*lDevice, indices.graphicsQueue.value, 0, &graphicQueue);
-  return graphicQueue;
+
+  // make queues
+  VkQueue graphicsQueue;
+  vkGetDeviceQueue(*lDevice, indices.graphicsQueue.value, 0, &graphicsQueue);
+
+  VkQueue presentationQueue;
+  vkGetDeviceQueue(*lDevice, indices.presentationQueue.value, 0,
+                   &presentationQueue);
+
+  // put in struct and return
+  handles.graphicsQueue = graphicsQueue;
+  handles.presentationQueue = presentationQueue;
+
+  return handles;
 }
